2e1acab4f6db92af83eb03ad42407f7f9c71cc13
Tristan Chu <tristan8chu@gmail.com>
2022-11-21 17:33:09 -0800

    timestamp logic implemented but not fully working and email should send to one person at a time now

--- just-move/functions/index.js
+++ just-move/functions/index.js
@@ -1,7 +1,7 @@
 //SendGrid Config
 // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 const functions = require('firebase-functions');
-
+const {Timestamp} = require("firebase-admin/firestore");
 // The Firebase Admin SDK to access Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -16,7 +16,7 @@ exports.scheduledFunction = functions.pubsub.schedule('every 2 minutes').onRun (
   let day = date.getDate();
   let month = date.getMonth() + 1;
   let year = date.getFullYear();
-  let currentDate = `${month}-${day}-${year}`;
+  let currentDate = `${year}-${month}-${day}`;
 
   //const db = admin.firestore();
   const emails = [];
@@ -32,6 +32,18 @@ exports.scheduledFunction = functions.pubsub.schedule('every 2 minutes').onRun (
     goal = data.goal;
     console.log(typeof(goal));
     console.log("goal: " + goal);
+    console.log("current reminder date: " + data.reminderDate);
+    const currReminderDateTimestamp = Timestamp.fromDate(new Date(data.reminderDate));
+    console.log(currReminderDateTimestamp);
+    console.log(typeof(currReminderDateTimestamp));
+    const currReminderDateMillis = Timestamp.toMillis(currReminderDateTimestamp.seconds);
+    const oneWeekMillis = 604800000;
+    const newReminderDateMillis = currReminderDateMillis + oneWeekMillis;
+    const newReminderDateTimestamp = Timestamp.fromMillis(newReminderDateMillis);
+    const newReminderDate = Timestamp.toDate(newReminderDateTimestamp);
+    console.log(newReminderDate);
+
+
     extrinsic = data.extrinsicMotivation;
     console.log("extrinsic: " + extrinsic);
     intrinsic = data.intrinsicMotivation;
@@ -44,25 +56,25 @@ exports.scheduledFunction = functions.pubsub.schedule('every 2 minutes').onRun (
     console.log(typeof(email));
     console.log(email);
     emails.push(email);
+
+    var msg = {
+      to: email,
+      from: 'tjchu@ucsc.edu',
+      templateId: TEMPLATE_ID,
+      dynamic_template_data: {
+        'name': email,
+        'goal': goal,
+        'extrinsic': intrinsic,
+        'intrinsic': extrinsic,
+      },
+    };
+    
+    console.log(msg);
+    console.log('This will run every day at 22:22');
+    console.log(context);
+    sgMail.send(msg);
   }
   console.log(emails);
-
-  var msg = {
-    to: emails,
-    from: 'tjchu@ucsc.edu',
-    templateId: TEMPLATE_ID,
-    dynamic_template_data: {
-      'name': email,
-      'goal': goal,
-      'extrinsic': intrinsic,
-      'intrinsic': extrinsic,
-    },
-  };
-  
-  console.log(msg);
-  console.log('This will run every day at 22:22');
-  console.log(context);
-  return sgMail.send(msg);
 });
 
 
