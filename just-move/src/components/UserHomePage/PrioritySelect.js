2b5787b7de5be0cb2fa9220ae0e10a1b0bf82540
Kieran Dawson <kldawson@ucsc.edu>
2022-11-29 14:05:00 -0800

    Fix priority select collapsing and expanding when clicked

--- just-move/src/components/UserHomePage/PrioritySelect.js
+++ just-move/src/components/UserHomePage/PrioritySelect.js
@@ -3,8 +3,13 @@ import React from "react";
 export const priorityStrings = ["----", "---", "--", "-", "=", "+", "++", "+++", "++++"];
 export const priorityRange = 4;
 
+const handleChildElementClick = (e) => {
+    e.stopPropagation();
+};
+
+
 export function PrioritySelect(props) {
-    return (<select name="priority" className={props.className} value={props.value} onChange={props.onChange}>
+    return (<select name="priority" className={props.className} value={props.value} onChange={props.onChange} onClick={handleChildElementClick}>
         {Array.from(Array(2 * priorityRange + 1).keys()).map((i) => (
             <option value={i - priorityRange} key={i}>{priorityStrings[i]}</option>
         ))}
